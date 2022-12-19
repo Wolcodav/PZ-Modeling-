ba676287938bc09f3df42b50bcf1db4f0c4e4261
Wolcodav <118798896+Wolcodav@users.noreply.github.com>
2022-12-20 00:36:33 +0300

    Add files via upload

---
+++ Zadanie 3/tools.py
@@ -0,0 +1,64 @@
+import math
+import numpy as np
+from ide import Source1D
+
+class GaussianDiff(Source1D):
+    '''
+    Источник, создающий дифференцированный гауссов импульс
+    '''
+
+    def __init__(self, magnitude, dg, wg):
+        '''
+        magnitude - максимальное значение в источнике;
+        dg - коэффициент, задающий начальную задержку гауссова импульса;
+        wg - коэффициент, задающий ширину гауссова импульса.
+        '''
+        self.magnitude = magnitude
+        self.dg = dg
+        self.wg = wg
+
+    def getE(self, v, time):
+        e = (time - self.dg) / self.wg
+        return - 2 * self.magnitude * e * np.exp(-(e ** 2))
+
+class LayerContinuous:
+    def __init__(self,
+                 xmin: float,
+                 xmax: float = None,
+                 eps: float = 1.0,
+                 mu: float = 1.0,
+                 sigma: float = 0.0):
+        self.xmin = xmin
+        self.xmax = xmax
+        self.eps = eps
+        self.mu = mu
+        self.sigma = sigma
+
+class LayerDiscrete:
+    def __init__(self,
+                 xmin: int,
+                 xmax: int = None,
+                 eps: float = 1.0,
+                 mu: float = 1.0,
+                 sigma: float = 0.0):
+        self.xmin = xmin
+        self.xmax = xmax
+        self.eps = eps
+        self.mu = mu
+        self.sigma = sigma
+
+class Sampler:
+    def __init__(self, discrete: float):
+        self.discrete = discrete
+
+    def sample(self, x: float) -> int:
+        return math.floor(x / self.discrete + 0.5)
+
+def sampleLayer(layer_cont: LayerContinuous, sampler: Sampler) -> LayerDiscrete:
+    start_discrete = sampler.sample(layer_cont.xmin)
+    end_discrete = (sampler.sample(layer_cont.xmax)
+                    if layer_cont.xmax is not None
+                    else None)
+    return LayerDiscrete(start_discrete, end_discrete,
+                         layer_cont.eps, layer_cont.mu, layer_cont.sigma)
+
